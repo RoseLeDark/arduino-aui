7da9ddaa3cfb5761e0c94290489139c8ccc97b82
Amber-Sophia Schröck <ambersophia.schroeck@mail.de>
2026-03-18 18:03:36 +0100

    add version

---
+++ src/AUI/aui_config.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <Arduino.h>
+
+
+#define AUI_CONFIG_ON                       1
+#define AUI_CONFIG_OFF                      0
+#define AUI_CONFIG_USED                     1
+
+#define AUI_SYSTEM_MAX_UI_ELEMENTS          8
+
+
+#if defined(__AVR__)
+    #define AUI_PLATFORM_AVR AUI_CONFIG_USED
+    #define AUI_ISR_ATTR
+#elif defined(ARDUINO_ARCH_SAMD)
+    #define AUI_PLATFORM_SAMD AUI_CONFIG_USED
+    #define AUI_ISR_ATTR
+#elif defined(ARDUINO_ARCH_STM32)
+    #define AUI_PLATFORM_STM32 AUI_CONFIG_USED
+    #define AUI_ISR_ATTR __attribute__((used))
+#elif defined(ESP32)
+    #define AUI_PLATFORM_ESP32 AUI_CONFIG_USED
+    #define AUI_ISR_ATTR ARDUINO_ISR_ATTR
+#else
+    #define AUI_ISR_ATTR
+#endif
+
+#if AUI_PLATFORM_SAMD == 1 || AUI_PLATFORM_STM32 == 1 || AUI_PLATFORM_ESP32 == 1
+#define AUI_CONFIG_EXT_INTERRUPT        AUI_CONFIG_ON
+#define AUI_CONFIG_ANALOG_INTERRUPT     AUI_CONFIG_ON
+#else 
+#define AUI_CONFIG_EXT_INTERRUPT        AUI_CONFIG_OFF
+#define AUI_CONFIG_ANALOG_INTERRUPT     AUI_CONFIG_OFF
+#endif
+
---
+++ src/AUI/aui_version.h
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "aui_config.h"
+
+#define AUI_VERSION_MAJOR            0
+#define AUI_VERSION_MINOR            3
+#define AUI_VERSION_REVISION         14
+
+// Freeze-Version: 0 = kein Freeze
+// Beispiel: 100 = freeze 1.0, 150 = freeze 1.5, 200 = freeze 2.0
+#define AUI_VERSION_API_FREEZE       0
+
+#ifndef AUI_VERSION_EXTNAME
+#define AUI_VERSION_EXTNAME           "dev"
+#endif
+
+#ifndef AUI_GIT_COMMENT
+#define AUI_GIT_COMMENT "__GIT__"
+#endif
+
+
+#define AUI_STRINGIFY2(x) #x
+#define AUI_STRINGIFY(x)  AUI_STRINGIFY2(x)
+
+#define AUI_FREEZE_MAJOR   (AUI_VERSION_API_FREEZE / 100)
+#define AUI_FREEZE_MINOR   ((AUI_VERSION_API_FREEZE % 100) / 10)
+
+#if AUI_VERSION_API_FREEZE == 0
+    #define AUI_FREEZE_STRING ""
+#else
+    #define AUI_FREEZE_STRING \
+        "api" \
+        AUI_STRINGIFY(AUI_FREEZE_MAJOR) "." \
+        AUI_STRINGIFY(AUI_FREEZE_MINOR)
+#endif
+
+#define AUI_VERSION_STRING \
+    AUI_STRINGIFY(AUI_VERSION_MAJOR) "." \
+    AUI_STRINGIFY(AUI_VERSION_MINOR) "." \
+    AUI_STRINGIFY(AUI_VERSION_REVISION) \
+    AUI_FREEZE_STRING "-" \
+    AUI_VERSION_EXTNAME
+
+#if AUI_GIT_COMMENT[0] == '\0'
+    #define AUI_VERSION_STRING_EXT ""
+#else
+    #define AUI_VERSION_STRING_EXT "_" AUI_GIT_COMMENT
+#endif
+
+#define AUI_VERSION_STRING \
+    AUI_STRINGIFY(AUI_VERSION_MAJOR) "." \
+    AUI_STRINGIFY(AUI_VERSION_MINOR) "." \
+    AUI_STRINGIFY(AUI_VERSION_REVISION) \
+    AUI_FREEZE_STRING "-" \
+    AUI_VERSION_EXTNAME \
+    AUI_VERSION_STRING_EXT
+
